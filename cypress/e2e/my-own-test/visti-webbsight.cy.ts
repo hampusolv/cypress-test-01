f0664df3fbfa727208bdfcd9f4df1f5818975bc5
Hampus Olviken <hampus.olviken@gmail.com>
2025-05-21 23:01:38 +0200

    add a loggin with wrong credentials

--- cypress/e2e/my-own-test/visti-webbsight.cy.ts
+++ cypress/e2e/my-own-test/visti-webbsight.cy.ts
@@ -1,15 +1,25 @@
 /// <reference types="cypress" />
+import { vi } from '@faker-js/faker/.';
 import { login } from '../../support/helper'
 
 beforeEach(() => {
-  login();
+    //  login();
 });
 
 describe('Login functionality', () => {
-  it('should log in with valid credentials', () => {
-    cy.url().should('include', '/inventory.html');
-    cy.get('.app_logo').should('contain.text', 'Swag Labs').and('be.visible');
-  });
+    it('should log in with valid credentials', () => {
+        login();
+        cy.url().should('include', '/inventory.html');
+        cy.get('.app_logo').should('contain.text', 'Swag Labs').and('be.visible');
+    });
+    it('should not log in with invalid credentials', () => {
+        cy.visit('https://www.saucedemo.com/');
+        cy.get('[data-test="username"]').clear().type('invalid_user');
+        cy.get('[data-test="password"]').clear().type('invalid_password');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('[data-test="error"]').contains('Epic sadface: Username and password do not match any user in this service').should('be.visible');
+    });
+
 });
 
 
